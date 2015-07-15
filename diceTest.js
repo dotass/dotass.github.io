ddac9dee44995290bac33ff98495d24ec12fec5f
dotass <danbobhead@yahoo.com>
2015-07-14 19:16:29 -0600

    2015-07-14 - Polished Dicetest
    - Declared all variables in one line for compiler.
    - Removed unnecessary function.
    - Removed unnecessary jQuery calls.

--- diceTest.js
+++ diceTest.js
@@ -1,25 +1,21 @@
 function clickTestScript(window, $) {
 	'use strict';
 
-	var $rollButton = $('.roll-button');
-	var $diceCount = $('.dice-count');
-	var $diceSize = $('.dice-size');
-
-	var diceSize = 6;
-	var diceQuantity = 1;
-
-	function rollIt() {
-		var diceResults = rollDice();
-	};
+	var $rollButton = $('.roll-button'),
+	    $diceCount = $('.dice-count'),
+	    $diceSize = $('.dice-size'),
+	    $diceResults = $('#dice-results'),
+	    diceSize = 6,
+	    diceQuantity = 1;
 
 	function setDiceCount() {
-		$('.active-number-of-dice').removeClass('active-number-of-dice');
+		$diceCount.removeClass('active-number-of-dice');
 		$(this).addClass('active-number-of-dice');
 		diceQuantity = $(this).attr('data-dice-quantity');
 	};
 
 	function setDiceSize() {
-		$('.active-dice-capacity').removeClass('active-dice-capacity');
+		$diceSize.removeClass('active-dice-capacity');
 		$(this).addClass('active-dice-capacity');
 		diceSize = $(this).attr('data-dice-size');
 	};
@@ -32,10 +28,10 @@ function clickTestScript(window, $) {
 
 			diceRollTotal = diceRollTotal + tempRoll;
 		};
-		$('#dice-results').text("The computer rolled "+diceQuantity+"d"+diceSize+", and rolled a "+diceRollTotal+".");
+		$diceResults.text("The computer rolled "+diceQuantity+"d"+diceSize+", and rolled a "+diceRollTotal+".");
 	};
 
-	$rollButton.on('click', rollIt);
+	$rollButton.on('click', rollDice);
 	$diceCount.on('click', setDiceCount);
 	$diceSize.on('click', setDiceSize);
 };
